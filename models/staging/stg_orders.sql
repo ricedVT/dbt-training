b6f1042314fe5bf9cba714e5759c4398bc8ea027
Dave Rice <riced@vt.edu>
2023-07-21 20:51:00 +0000

    added delivery_team seed and added to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -17,9 +17,12 @@ p.category,
 p.productid,
 p.productname,
 p.subcategory,
-{{ markup('ordersellingprice','ordercostprice') }} as markup
+{{ markup('ordersellingprice','ordercostprice') }} as markup,
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+left join {{ ref('delivery_team')}} as d
+on o.shipmode = d.shipmode
